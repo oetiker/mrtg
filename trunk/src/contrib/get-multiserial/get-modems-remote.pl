ee699bb8e58ed372e01abc9969f04491c016762a
oetiker <oetiker@12652d4d-800b-0410-8ec2-e5fdc4a57536>
2006-01-29 16:02:07 +0000

    version 2.9.29 checkin

--- trunk/src/contrib/get-multiserial/get-modems-remote.pl
+++ trunk/src/contrib/get-multiserial/get-modems-remote.pl
@@ -1,21 +1,29 @@
 #! /usr/bin/perl
 #
+# get-modems-remote.pl
+#
 # Connects to a TCP port get's there the number of occupied modems
 # and prints'em out
 #
 # The other side consists of a silly programm (a la get-modems.pl ;)
 # that just prints the number of active lines.
+#
 # ex:
 # /etc/services :
 #   getmodems	9999/tcp
-# /etc/inetd.conf
-#   getmodems          stream  tcp     nowait  nobody  /usr/sbin/get-modems.pl get-modems.pl
-# 
+# /etc/inetd.conf:
+#   getmodems  stream  tcp  nowait  nobody  /usr/bin/get-modems.pl get-modems.pl
 #
 # Thanks to Larry Wall for the nice example in the Lama Perl-tutorial
 #
-# (cl) T.Pospisek <tpo@spin.ch> Distributed under GNU copyleft
+# (c) T.Pospisek <tpo@spin.ch> Distributed under GNU copyleft
 #
+# History:
+# ??-??-97	initial release for mrtg
+# 13-05-99	cleaned up a little bit - thanks to Dancho Kanev
+#               <dancho@globcom.net> for "buging" me ;-)
+
+# ---- please adapt the following two lines to your local configuration ---
 
 # The name of the remote server
 $remote_server='jesus';
@@ -23,10 +31,10 @@ $remote_server='jesus';
 # The name of the service
 $service='getmodems';
 
-# where's sys/socket.ph
-$SOCKET_PH = '/usr/lib/perl5/i486-linux/5.003/linux/socket.ph';
+#-- end of user configurable stuff     ---
 
-require $SOCKET_PH;
+use Socket;
+
 $sockaddr = 'S n a4 x8';
 chop($hostname = `hostname`);
 ($name, $aliases, $proto) = getprotobyname('tcp');
