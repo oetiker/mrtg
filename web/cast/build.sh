69c348fd3e79c29a841fae0ae9ed614ddb3cb818
Tobias Oetiker <tobi@oetiker.ch>
2006-09-14 07:17:56 +0000

    updates to get going on non eth host

--- web/cast/build.sh
+++ web/cast/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-perl -n -e 's/Tobi//;s/\s*[&+]\s*//;s/\s+and\s+//; /^From:\s*(\S+\s\S+)/i && do { $x=$1; $x =~ s/\s*<.+//;$x =~ s/"//g; $x=~ s/\s*location\s*$//; print "$x;\n"}' /home/oetiker/data/svn-checkout/mrtg/trunk/src/CHANGES | sort -u >contrib.inc
+perl -n -e 's/Tobi//;s/\s*[&+]\s*//;s/\s+and\s+//; /^From:\s*(\S+\s\S+)/i && do { $x=$1; $x =~ s/\s*<.+//;$x =~ s/"//g; $x=~ s/\s*location\s*$//; print "$x;\n"}' /home/oetiker/checkouts/mrtg/trunk/src/CHANGES | sort -u >contrib.inc
