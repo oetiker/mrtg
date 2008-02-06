6ee6cfb734781b389b2d3492cdcd6483bcb69e93
Tobias Oetiker <tobi@oetiker.ch>
2008-02-06 14:32:42 +0000

    released version 2.16.0

--- src/src/rateup.c
+++ src/src/rateup.c
@@ -1,5 +1,5 @@
 /*
- MRTG 2.15.2  -- Rateup
+ MRTG 2.16.0  -- Rateup
  *********************
 
  Rateup is a fast add-on to the great MRTG Traffic monitor.  It makes
@@ -83,7 +83,7 @@
 #include <gd.h>
 #include <gdfonts.h>
 
-char *VERSION = "2.15.2";
+char *VERSION = "2.16.0";
 char *program, *router, *routerpath;
 int histvalid;
 
