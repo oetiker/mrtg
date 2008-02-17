e2f2b08ff74d5498ceed53ffd545ab8cf1096438
Tobias Oetiker <tobi@oetiker.ch>
2008-02-17 10:27:50 +0000

    released version 2.16.1

--- src/lib/mrtg2/MRTG_lib.pm
+++ src/lib/mrtg2/MRTG_lib.pm
@@ -1,7 +1,7 @@
 # -*- mode: Perl -*-
 package MRTG_lib;
 ###################################################################
-# MRTG 2.16.0  Support library MRTG_lib.pm
+# MRTG 2.16.1  Support library MRTG_lib.pm
 ###################################################################
 # Created by Tobias Oetiker <tobi@oetiker.ch>
 #            and Dave Rand <dlr@bungi.com>
