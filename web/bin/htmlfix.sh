60868b0f4d13228440171769927a1e8ccb0a4a3b
Tobias Oetiker <tobi@oetiker.ch>
2009-06-29 07:47:39 +0000

    added servercheck

--- web/bin/htmlfix.sh
+++ web/bin/htmlfix.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 case $1 in
 *.html)
-   tidy -latin1 -wrap 0 -q -asxhtml $1  >$1.fixed  2>$1.report
+#   tidy -latin1 -wrap 0 -q -asxhtml $1  >$1.fixed  2>$1.report
 #   TIDY_RET=$?
+   exit 0
    TIDY_RET=0
    perl -i -0777 -p -e 's/^\s*//;s{="mailto:(oetiker|tobi|tobias)@(oetiker.ch|ee.ethz.ch)"}{="http://tobi.oetiker.ch/"}g;s{="mailto:(\S*?)\@(\S*?)"}{="mailto:$1@..delete..this..$2"}g' $1.fixed
    # yes, beleive it or not IE chockes on propper xhtml pages ... sigh
