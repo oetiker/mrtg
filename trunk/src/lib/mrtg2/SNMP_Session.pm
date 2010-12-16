5d5b6a0f684e124149ab71714834bae58cecb026
oetiker <oetiker@12652d4d-800b-0410-8ec2-e5fdc4a57536>
2010-12-16 13:43:37 +0000

    explicitly import Socket6 routines in SNMP_Session

--- trunk/src/lib/mrtg2/SNMP_Session.pm
+++ trunk/src/lib/mrtg2/SNMP_Session.pm
@@ -146,7 +146,7 @@ BEGIN {
 
     if (eval {local $SIG{__DIE__};require Socket6;} &&
        eval {local $SIG{__DIE__};require IO::Socket::INET6; IO::Socket::INET6->VERSION("1.26");}) {
-	import Socket6;
+       Socket6->import(qw(pack_sockaddr_in6 inet_pton getaddrinfo));
 	$ipv6_addr_len = length(pack_sockaddr_in6(161, inet_pton(AF_INET6(), "::1")));
 	$SNMP_Session::ipv6available = 1;
     }
@@ -600,8 +600,8 @@ use Carp;
 
 BEGIN {
     if($SNMP_Session::ipv6available) {
-	import IO::Socket::INET6;
-	import Socket6;
+	    import IO::Socket::INET6;
+        Socket6->import(qw(pack_sockaddr_in6 inet_pton getaddrinfo));
     }
 }
 
