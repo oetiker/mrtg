f70bc9b1e2468830d5183f0bfb0c154785b726d0
Tobias Oetiker <tobi@oetiker.ch>
2012-08-22 09:45:29 +0000

    do not import unpack_sockaddr_in6 as this seems to generate conflicts, rather access the function under it's full name

--- src/lib/mrtg2/SNMP_Session.pm
+++ src/lib/mrtg2/SNMP_Session.pm
@@ -146,7 +146,7 @@ BEGIN {
 
     if (eval {local $SIG{__DIE__};require Socket6;} &&
        eval {local $SIG{__DIE__};require IO::Socket::INET6; IO::Socket::INET6->VERSION("1.26");}) {
-       Socket6->import(qw(pack_sockaddr_in6 inet_pton getaddrinfo unpack_sockaddr_in6));
+       Socket6->import(qw(pack_sockaddr_in6 inet_pton getaddrinfo));
 	$ipv6_addr_len = length(pack_sockaddr_in6(161, inet_pton(AF_INET6(), "::1")));
 	$SNMP_Session::ipv6available = 1;
     }
@@ -545,7 +545,7 @@ sub pretty_address {
     # complaining about AF_INET6 when Socket6 is not available
 
     if( (defined $ipv6_addr_len) && (length $addr == $ipv6_addr_len)) {
-	($port,$addrunpack) = unpack_sockaddr_in6 ($addr);
+	($port,$addrunpack) = Socket6::unpack_sockaddr_in6 ($addr);
 	$addrstr = inet_ntop (AF_INET6(), $addrunpack);
     } else {
 	($port,$addrunpack) = unpack_sockaddr_in ($addr);
@@ -601,7 +601,7 @@ use Carp;
 BEGIN {
     if($SNMP_Session::ipv6available) {
 	    import IO::Socket::INET6;
-        Socket6->import(qw(pack_sockaddr_in6 inet_pton getaddrinfo unpack_sockaddr_in6));
+        Socket6->import(qw(pack_sockaddr_in6 inet_pton getaddrinfo));
     }
 }
 
@@ -820,8 +820,8 @@ sub sa_equal_p ($$$) {
 	($p2,$a2) = unpack_sockaddr_in ($sa2);
     } elsif($this->{'sockfamily'} == AF_INET6()) {
 	# IPv6 addresses
-	($p1,$a1) = unpack_sockaddr_in6 ($sa1);
-	($p2,$a2) = unpack_sockaddr_in6 ($sa2);
+	($p1,$a1) = Socket6::unpack_sockaddr_in6 ($sa1);
+	($p2,$a2) = Socket6::unpack_sockaddr_in6 ($sa2);
     } else {
 	return 0;
     }
@@ -894,7 +894,7 @@ sub receive_trap {
     return undef unless $remote_addr;
 
     if( (defined $ipv6_addr_len) && (length $remote_addr == $ipv6_addr_len)) {
-	($port,$iaddr) = unpack_sockaddr_in6($remote_addr);
+	($port,$iaddr) = Socket6::unpack_sockaddr_in6($remote_addr);
     } else {
 	($port,$iaddr) = unpack_sockaddr_in($remote_addr);
     }
@@ -943,7 +943,7 @@ sub receive_request {
     return undef unless $remote_addr;
 
     if( (defined $ipv6_addr_len) && (length $remote_addr == $ipv6_addr_len)) {
-	($port,$iaddr) = unpack_sockaddr_in6($remote_addr);
+	($port,$iaddr) = Socket6::unpack_sockaddr_in6($remote_addr);
     } else {
 	($port,$iaddr) = unpack_sockaddr_in($remote_addr);
     }
